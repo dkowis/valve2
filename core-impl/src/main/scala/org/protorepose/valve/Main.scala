d3ea497085716b461bc8e90a7f10f873306daa5f
David Kowis <david.kowis@rackspace.com>
2014-09-16 17:18:23 -0500

    Made some substantial changes

--- core-impl/src/main/scala/org/protorepose/valve/Main.scala
+++
@@ -1,8 +0,0 @@
-package org.protorepose.valve
-
-
-object Main extends App {
-  println("WOOT")
-
-  //Create a spring context for each jetty, and share some parent context that includes some "service"
-}
--- core-impl/src/main/scala/org/protorepose/valve/coreService/CoreServiceImpl.scala
+++
@@ -1,7 +0,0 @@
-package org.protorepose.valve.coreService
-
-class CoreServiceImpl extends CoreService {
-  override def coreServiceThingy(string: String): Unit = {
-    println(s"CORE SERVICE CALLED WITH $string")
-  }
-}
