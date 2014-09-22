6492eda244d9bf5a84e8d2cc15f19c37425ce4b6
David Kowis <david.kowis@rackspace.com>
2014-09-22 15:46:37 -0500

    No more this class

--- valve/src/main/scala/org/protorepose/valve/SpringConfig.scala
+++
@@ -1,9 +0,0 @@
-package org.protorepose.valve
-
-import org.springframework.context.annotation.{ComponentScan, Configuration}
-
-@Configuration
-@ComponentScan(Array("org.protorepose"))
-class SpringConfig {
- //Shouldn't need anything in here
-}
